a68edf5a0367d6360b9473252e39323d0f961885
oriom <47474097+oriom-dev@users.noreply.github.com>
2026-04-11 15:29:16 +0900

    fix: hello world without secret

--- containers/hello-world/default.nix
+++ containers/hello-world/default.nix
@@ -5,19 +5,13 @@ let
   ossCommitHash = "d9435b5a2e57ecf7b49bd613e54b67ce4d98a280";
   
   # 共通サイドカーテンプレートと、固有テンプレートの読み込み
-  sidecarTemplate = builtins.readFile ../shared-templates/app-tailscale.container.in;
+  sidecarTemplate = builtins.readFile ../../templates/http/tailscale.container.in;
   appTemplate = builtins.readFile ./hello-world.container.in;
   containerfileTemplate = builtins.readFile ./Containerfile.in;
   
-  homeDir = config.home.homeDirectory;
   secretsPath = config.sops.secrets."${appName}_env".path;
 in
 {
-  sops.secrets."${appName}_env" = {
-    sopsFile = ./secrets.yaml;
-    format = "yaml";
-  };
-
   home.file = {
     # tailscale sidecar
     ".config/containers/systemd/${appName}-tailscale.container".text = 
@@ -26,8 +20,8 @@ in
     # app quadlet
     ".config/containers/systemd/${appName}.container".text = 
       builtins.replaceStrings 
-        [ "@APP_NAME@" "@HOME_DIR@" "@SECRETS_PATH@" "@COMMIT_HASH@" ] 
-        [ appName homeDir secretsPath ossCommitHash ] 
+        [ "@APP_NAME@" "@COMMIT_HASH@" ] 
+        [ appName ossCommitHash ] 
         appTemplate;
 
     # container
