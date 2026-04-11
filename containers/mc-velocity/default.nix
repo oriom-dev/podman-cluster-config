b63873ce3033af7b82e27bfed62bd4294eb3d318
oriom <47474097+oriom-dev@users.noreply.github.com>
2026-04-11 14:50:09 +0900

    fix: embed velocity secret via template

--- containers/mc-velocity/default.nix
+++ containers/mc-velocity/default.nix
@@ -13,18 +13,19 @@ in
     format = "yaml";
   };
 
+  sops.templates."velocity.toml" = {
+    path = "${config.home.homeDirectory}/.config/velocity/velocity.toml";
+    content = builtins.replaceStrings 
+      [ "@FORWARDING_SECRET@" ] 
+      [ "<sops:${appName}_secrets>" ] 
+      velocityTemplate;
+  };
+
   home.file = {
     # サイドカー (静的ファイルとして配置)
     ".config/containers/systemd/${appName}-tailscale.container".source = ./tailscale.container;
 
     # 本体 (静的ファイルとして配置)
     ".config/containers/systemd/${appName}.container".source = ./mc-velocity.container;
-
-    # 設定ファイル (パスワードを注入して配置)
-    ".config/velocity/velocity.toml".text = 
-      builtins.replaceStrings 
-        [ "@FORWARDING_SECRET@" ] 
-        [ config.sops.placeholder."${appName}_secrets" ] # sops-nixが復号後の値に置換します
-        velocityTemplate;
   };
 }
