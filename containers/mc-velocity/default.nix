1e60230183b9536ecf9ef3ddca4232aee5c4ec59
Morio Chiba <47474097+oriom-dev@users.noreply.github.com>
2026-04-11 12:59:13 +0900

    velocity

--- containers/mc-velocity/default.nix
+++ containers/mc-velocity/default.nix
@@ -1,8 +1,30 @@
 { config, pkgs, ... }:
+
+let
+  appName = "mc-velocity";
+  
+  # velocity.toml のテンプレートを読み込み
+  velocityTemplate = builtins.readFile ./velocity.toml.in;
+in
 {
+  # このコンテナ専用のシークレットを定義
+  sops.secrets."${appName}_secrets" = {
+    sopsFile = ./secrets.yaml;
+    format = "yaml";
+  };
+
   home.file = {
-    ".config/containers/systemd/mc-velocity.container".source = ./mc-velocity.container;
-    ".config/containers/systemd/tailscale.container".source = ./tailscale.container;
-    ".config/velocity/velocity.toml".source = ./velocity.toml;
+    # サイドカー (静的ファイルとして配置)
+    ".config/containers/systemd/${appName}-tailscale.container".source = ./tailscale.container;
+
+    # 本体 (静的ファイルとして配置)
+    ".config/containers/systemd/${appName}.container".source = ./mc-velocity.container;
+
+    # 設定ファイル (パスワードを注入して配置)
+    ".config/velocity/velocity.toml".text = 
+      builtins.replaceStrings 
+        [ "@FORWARDING_SECRET@" ] 
+        [ config.sops.placeholder."${appName}_secrets" ] # sops-nixが復号後の値に置換します
+        velocityTemplate;
   };
 }
