5f5d89dd09f76c1f09e114047b61563107bd1112
oriom <47474097+oriom-dev@users.noreply.github.com>
2026-04-18 16:58:08 +0900

    fix: velocity without tailscale

--- containers/mc-velocity/default.nix
+++ containers/mc-velocity/default.nix
@@ -7,20 +7,9 @@ in
 {
   sops.secrets."mc-velocity_forwarding-secret" = { sopsFile = ./secrets.yaml; format = "yaml"; };
 
-  # 自動起動リストへの申告
-  podman.activeServices = [ "${appName}-tailscale" appName ];
+  podman.activeServices = [ appName ];
 
   home.file = 
-    # サイドカー
-    helper.mkQuadlet {
-      name = "${appName}-tailscale";
-      templatePath = ./tailscale.container;
-      vars = {
-        "@TS_SECRET_PATH@" = config.podman.tailscaleAuthKeyPath;
-        "@TS_LOGIN_SERVER@" = config.podman.tailscaleLoginServer;
-      };
-    } // 
-    # 本体
     helper.mkQuadlet {
       name = appName;
       templatePath = ./mc-velocity.container;
